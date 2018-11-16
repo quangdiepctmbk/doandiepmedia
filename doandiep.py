5b10f28645c7ba6534b06fb6d80c54c1a9d6c888
quangdiepctmbk <44939579+quangdiepctmbk@users.noreply.github.com>
2018-11-16 09:12:29 +0700

    Update doandiep.py

--- doandiep.py
+++ doandiep.py
@@ -35,7 +35,7 @@ sheet_headers = {
 
 
 def GetSheetIDFromSettings():
-	sid = "1zL6Kw4ZGoNcIuW9TAlHWZrNIJbDU5xHTtz-o8vpoJss"
+	sid = "13zUxgD6SfGj1YmRa9RU4Vu1AgVv8EaBAsBP3MxEpXOY"
 	resp, content = http.request(plugin.get_setting("GSheetURL"), "HEAD")
 	try:
 		sid = re.compile("/d/(.+?)/").findall(resp["content-location"])[0]
