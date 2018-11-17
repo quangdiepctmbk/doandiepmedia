751017b05dece7672f0af7a2289cfb22e04d4251
quangdiepctmbk <44939579+quangdiepctmbk@users.noreply.github.com>
2018-11-18 06:55:51 +0700

    Update doandiep.py

--- doandiep.py
+++ doandiep.py
@@ -1052,7 +1052,7 @@ def get_playable_url(url):
         'Connection': 'keep-alive'
         }
         (resp, content) = http.request(url,"GET",headers=headers)
-        match = re.findall(r'\"(http://data36.chiasenhac.com.*?720p\]\.mp4)\"', content)
+        match = re.search(r'\"(http://data36.chiasenhac.com.*?720p\]\.mp4)\"', content)
         return match		
 	else:
 		if "://" not in url:
