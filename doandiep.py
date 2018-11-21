bf7fa0390c062b7759be8ab33268fec6d554b77b
quangdiepctmbk <44939579+quangdiepctmbk@users.noreply.github.com>
2018-11-21 20:38:38 +0700

    Update doandiep.py

--- doandiep.py
+++ doandiep.py
@@ -1045,17 +1045,16 @@ def get_playable_url(url):
 		did = re.compile("/(\w+)$").findall(url)[0]
 		return "plugin://plugin.video.dailymotion_com/?url=%s&mode=playVideo" % did
 	elif "chiasenhac" in url:
-		#url ='http://chiasenhac.vn/nhac-hot/dap-mo-cuoc-tinh~dan-nguyen-quang-le-bang-kieu~tsvbvm6vqq28k2.html'
+		url ='http://chiasenhac.vn/nhac-hot/dap-mo-cuoc-tinh~dan-nguyen-quang-le-bang-kieu~tsvbvm6vqq28k2.html'
 		#url = urllib.parse.quote_plus(url)
-		#url= urllib.parse.unquote_plus(url)
-		#headers = {
-		#	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
-		#	'Accept-Encoding': 'gzip, deflate'
-		#}
-		#(resp,content) = http.request(url,"GET",headers=headers)
-		#match = re.findall(r'\"(http://data36.chiasenhac.com.*?720p\]\.mp4)\"', content.decode('utf-8'))
-		url = "http://data36.chiasenhac.com/downloads/1966/3/1965641-57b4cabe/m4a/Dap%20Mo%20Cuoc%20Tinh%20-%20Dan%20Nguyen_%20Quang%20Le_ [MP4 HD 720p].mp4"
-		#return match
+		url= urllib.parse.unquote_plus(url)
+		headers = {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
+			'Accept-Encoding': 'gzip, deflate'
+		}
+		(resp,content) = http.request(url,"GET",headers=headers)
+		match = re.findall(r'\"(http://data36.chiasenhac.com.*?720p\]\.mp4)\"', content.decode('utf-8'))
+		return match[0]
 	else:
 		if "://" not in url:
 			url = None
