affad6b5b426d7dce5a344122934f17e37fa0b69
quangdiepctmbk <44939579+quangdiepctmbk@users.noreply.github.com>
2018-12-02 22:08:28 +0700

    Update doandiep.py

--- doandiep.py
+++ doandiep.py
@@ -1052,7 +1052,7 @@ def get_playable_url(url):
 		(resp, content) = http.request(url,"GET",headers=headers)
 		#response = urlfetch.get(url, headers=headers)
 		match = re.findall(r'\"(http.*?chiasenhac.com.*?720p\]\.mp4)\"', content.decode('utf-8'))
-		match = match[0].replace(" ","%20")
+		match = match[-1].replace(" ","%20")
 		return match
 	else:
 		if "://" not in url:
